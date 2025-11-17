704e6a5ed9f04da7d121d9290c482d42b4c25a7b
Santiago Suárez Rojas ZFS <zenonfansub@gmail.com>
2025-11-17 08:55:18 -0500

    refactor: clean public folder and references

--- src/app/components/tips/tights-combination/tights-combination.ts
+++ src/app/components/tips/tights-combination/tights-combination.ts
@@ -22,17 +22,17 @@ export class TightsCombination {
           description: 'Medias veladas efecto Beyoncé color claro',
         },
         {
-          imageUrl: '/tips/tights-combination/skin-tone-light-1.2.PNG',
+          imageUrl: '/tips/tights-combination/skin-tone-light-1.2.png',
           imageAlt: 'skin-tone-light-1.2',
           description: 'Medias malla profesionales color claro',
         },
         {
-          imageUrl: '/tips/tights-combination/skin-tone-light-1.3.PNG',
+          imageUrl: '/tips/tights-combination/skin-tone-light-1.3.png',
           imageAlt: 'skin-tone-light-1.3',
           description: 'Medias malla profesionales color canela',
         },
         {
-          imageUrl: '/tips/tights-combination/skin-tone-light-1.4.PNG',
+          imageUrl: '/tips/tights-combination/skin-tone-light-1.4.png',
           imageAlt: 'skin-tone-light-1.4',
           description:
             'Medias veladas efecto Beyoncé color claro<br/>+<br/>Medias malla profesionales color canela',
