b3b18e5bdc00e988835c1f17aa26c52a453abf0b
Santiago Suárez Rojas ZFS <zenonfansub@gmail.com>
2025-12-09 08:15:45 -0500

    feat: Update Wompi configuration for production environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,6 +4,11 @@ export const environment = {
   ecommerceUrl: 'https://api.zcdanceshop.com/ecommerce/v1',
   // geolocationUrl: 'http://localhost:12001/v1',
   geolocationUrl: 'https://api.zcdanceshop.com/geolocation/v1',
+  // wompi: {
+  //   publicKey: 'pub_prod_bu6KwOqH1oifYDYH8NC9KBRlLEOn3DTW',
+  //   integrity: 'prod_integrity_UqTmkRxZHje6GbSx3krrjsrH3hDmb4QT'
+  // }
+
   wompi: {
     publicKey: 'pub_test_dh8B4Jc20RO5iNnEk42kSgF0APJcA1ia',
     integrity: 'test_integrity_JRFc1P3x77NV1t6X9DRGvCovxYtUlq5u',
